d2f445113ce5c884d924624495876b1fdae92607
Tala-Hodeib <talahodeib@gmail.com>
2020-09-30 19:58:36 +0300

    step 7 basics 1 unfinished

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,9 @@
+var size = prompt("What is your shoe size?");
+var birth = prompt ("What year were you born on?");
+function myFunction(size,birth){
+    return size*2
+    
+
+}
+myFunction(41,1992);
+alert(myFunction);
